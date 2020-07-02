113a286482ce60809fa744a518c94a0fbf94985b
Navonil Das <navneeladas@gmail.com>
2020-07-02 14:03:28 +0530

    Option to choose Framerate and algo

--- sketch.js
+++ sketch.js
@@ -3,16 +3,28 @@ function setup() {
 var width = 0;
 var height = 0;
 var algo;
-function Start() {
+
+function Start(algotype,rate = 5) {
+    const algomap = [
+        "HuntAndKill",
+        "Backtracker",
+        "Kruskal",
+        "Prims",
+        "GrowingTree",
+        "GrowingBinaryTree"
+    ];
     noCanvas();
     width = cellSize * cols;
     height = cellSize * rows;
     let cnv = createCanvas(width + 20, height + 20);
     cnv.parent('myWorkspace');
+    if(rate){
+        frameRate(rate);
+    }
     strokeWeight(4);
     translate(10, 10);
 
-    algo = new Kruskal();
+    algo = new window[algomap[algotype]]();
     algo.setup();
 }
 
